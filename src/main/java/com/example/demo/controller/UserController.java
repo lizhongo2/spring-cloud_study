99eed5d95cd7219b6cd78e764d855237f2872618
lizhong_o2 <lizhong@163.com>
2019-07-21 12:57:21 +0800

    tiojiao

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @RequestMapping("hello")
+    public String hello(String userName){
+        return "hello:"+userName;
+    }
+
+}
