f4ef583b442c959a79067dc332974e51efd73f39
lizhongo2 <lizhong_o2@163.com>
2019-08-04 22:18:04 +0800

    缓存提交

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.CacheManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping("hello")
-    public String hello(String userName){
-        return "hello:"+userName;
+    public String hello(String userName) {
+        CacheManager cacheManager = BeanUtils.applicationContext.getBean(CacheManager.class);
+        logger.info(cacheManager.getClass().getName());
+        return "hello:" + userName;
     }
 
 }
